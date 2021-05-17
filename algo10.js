96ae9521b146dd9505dd5dbc6eb8026f85e47026
Peter <78966864+phellwege@users.noreply.github.com>
2021-05-17 10:56:40 -0600

    Add files via upload

---
+++ algo10.js
@@ -0,0 +1,69 @@
+// Interview Algo given to alumni
+
+/* 
+  You are given a list of integers. Find all the consecutive sets of 
+  integers that adds up to the sum passed in as one of the inputs.
+*/
+
+const nums1 = [2, 5, 3, 6, 7, 23, 12];
+const sum1 = 16;
+const expected1 = [
+    [2, 5, 3, 6],
+    [3, 6, 7],
+];
+
+// Bonus:
+const nums2 = [2, 5, 3, 6, 7, 0, 0, 23, 12];
+const sum2 = 16;
+const expected2 = [
+    [2, 5, 3, 6],
+    [3, 6, 7],
+    [3, 6, 7, 0],
+    [3, 6, 7, 0, 0],
+];
+
+// Bonus:
+const nums3 = [-2, -5, -3, -6, -7, -0, -0, -23, -12];
+const sum3 = -16;
+const expected3 = [
+    [-2, -5, -3, -6],
+    [-3, -6, -7],
+    [-3, -6, -7, -0],
+    [-3, -6, -7, -0, -0],
+];
+
+/**
+ * Finds all the sets of consecutive numbers that sum to the given target sum.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums Unordered nums.
+ * @param {number} targetSum
+ * @returns {Array<Array<number>>} 2d array where each nested array is a set of
+ *    consecutive numbers that add up to the given targetSum. Consecutive in
+ *    this context means the numbers whose indexes are one after the other
+ *    only.
+ */
+function findConsqSums(nums, targetSum) {
+    //loop through array
+    //have a temp array keep pushing values into temp array as long as their sum is < the given sum
+    //if the array sum = given sum then push temp array into result array
+    //if greater then reset temp array
+    let resultArray = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let tempSum = 0;
+        let tempArray = [];
+        for (let j = i; j < nums.length; j++) {
+            tempArray.push(nums[j]);
+            tempSum += nums[j];
+            if (tempSum == targetSum) {
+                resultArray = [[...resultArray, tempArray]];
+                resultArray.push([...tempArray]);
+            }
+        }
+    }
+    return resultArray;
+}
+console.log(findConsqSums(nums1, sum1));
+console.log(findConsqSums(nums2, sum2));
+console.log(findConsqSums(nums3, sum3));
